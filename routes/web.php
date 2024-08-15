f130453aadd87665b82fde06d639940486da870d
rahal13001 <rahal13001@mail.unpad.ac.id>
2024-08-15 09:00:38 +0700

    update logout feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Volt::route('/', 'users.index');
+
+Route::get('/logout', function (Request $request) {
+    Auth::logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect('/');
+});
